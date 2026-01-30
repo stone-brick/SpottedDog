dcb9fa5aa03cb1d3045c43e3610a697881840dac
stone-brick <stone_brick@163.com>
2026-01-30 16:27:21 +0800

    Revert "fix: 修复 Spot 列表表头与内容对齐问题"
    This reverts commit 3d484aae1b7309acd3d30e855f63023de79ca475.

--- src/client/java/io/github/stone_brick/spotteddog/client/command/SpotCommand.java
+++ src/client/java/io/github/stone_brick/spotteddog/client/command/SpotCommand.java
@@ -433,8 +433,7 @@ public class SpotCommand {
     private static final int COL_VIS = 6;
 
     private static void sendTableHeader() {
-        // 使用与内容相同的颜色代码格式以保持对齐
-        String header = String.format("§l%-"+COL_NAME+"s §b%-"+COL_DIM+"s §f%-"+COL_COORD+"s %-"+COL_VIS+"s",
+        String header = String.format("§l%-"+COL_NAME+"s %-"+COL_DIM+"s %-"+COL_COORD+"s %-"+COL_VIS+"s",
                 Text.translatable("spotteddog.list.header.name").getString(),
                 Text.translatable("spotteddog.list.header.dimension").getString(),
                 Text.translatable("spotteddog.list.header.coord").getString(),
